70076dcac744fb938cfa13e093500642de537ac7
jangamsravanthi <113700033+jangamsravanthi@users.noreply.github.com>
2024-11-05 09:45:55 +0530

    Update App.java

--- BankingMicroService/src/main/java/com/sravanthi/App.java
+++ BankingMicroService/src/main/java/com/sravanthi/App.java
@@ -5,6 +5,6 @@ package com.sravanthi;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Sravanthi!!!");
     }
 }
